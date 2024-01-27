e416452c98447135a9b6b62ab534a832ee258947
mey21009 <mey2100@byui.edu>
2024-01-27 02:08:32 -0700

    sprint1

---
+++ sprint1/Main.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+class Movie {
+    private String title;
+    private List<String> genres;
+
+    public Movie(String title, List<String> genres) {
+        this.title = title;
+        this.genres = genres;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+}
+
+class User {
+    private List<String> interests;
+
+    public User(List<String> interests) {
+        this.interests = interests;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+}
+
+class MovieRecommendationSystem {
+    private List<Movie> movies;
+
+    public MovieRecommendationSystem(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public List<Movie> recommendMovies(User user) {
+        List<Movie> recommendedMovies = new ArrayList<>();
+
+        // Collect user interests
+        Set<String> userInterests = new HashSet<>(user.getInterests());
+
+        // Find movies that match user interests
+        for (Movie movie : movies) {
+            if (Collections.disjoint(userInterests, movie.getGenres())) {
+                continue; // Skip if the movie's genres don't match user interests
+            }
+            recommendedMovies.add(movie);
+        }
+
+        return recommendedMovies;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Sample movies
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("The Shawshank Redemption", Arrays.asList("Drama")));
+        movies.add(new Movie("The Godfather", Arrays.asList("Drama", "Crime")));
+        movies.add(new Movie("Inception", Arrays.asList("Action", "Adventure", "Sci-Fi")));
+        movies.add(new Movie("Pulp Fiction", Arrays.asList("Crime", "Drama")));
+        movies.add(new Movie("The Heffalump Movie", Arrays.asList("Animated", "Adventure")));
+        movies.add(new Movie("Robin Hood ", Arrays.asList("Animated", "Adventure")));
+        movies.add(new Movie("Beauty and the Beast", Arrays.asList("Adventure", "Fantasy")));
+        movies.add(new Movie("The Hobbit: An Unexpected Journey", Arrays.asList("Adventure", "Fantasy")));
+        movies.add(new Movie("The Hobbit: The Desolation of Smaug", Arrays.asList("Adventure", "Fantasy")));
+        movies.add(new Movie("The Hobbit: The Battle of the Five Armies", Arrays.asList("Adventure", "Fantasy")));
+        movies.add(new Movie("The Count of Monte Cristo ", Arrays.asList("Adventure", "Animation")));
+        movies.add(new Movie("Frequency", Arrays.asList("Crime", "Mystery")));
+        movies.add(new Movie("Field of Dreams", Arrays.asList("Drama")));
+        movies.add(new Movie("Toy Story", Arrays.asList("Animation", "Action", "Comedy")));
+        movies.add(new Movie("Frozen", Arrays.asList("Animation", "Action", "Comedy", "Musical" )));
+        movies.add(new Movie("The Incredibles", Arrays.asList("Adventure", "Animation", "Action" )));
+        movies.add(new Movie("Up", Arrays.asList("Adventure", "Animation", "Action" )));
+        movies.add(new Movie("Coco", Arrays.asList("Animation","Adventure", "Family")));
+        movies.add(new Movie("Moana", Arrays.asList("Animation", "Action", "Comedy", "Musical" )));
+        movies.add(new Movie("How to Train Your Dragon", Arrays.asList("Adventure", "Animation", "Action" )));
+        movies.add(new Movie("Zootopia", Arrays.asList("Animation”, “Adventure” ,“Comedy")));
+
+
+        // Prompt user to input interests
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your interests (separated by commas):");
+        String input = scanner.nextLine();
+        List<String> interests = Arrays.asList(input.split("\\s*,\\s*"));
+
+        // Create user object with interests
+        User user = new User(interests);
+
+        // Create recommendation system and get recommended movies
+        MovieRecommendationSystem recommendationSystem = new MovieRecommendationSystem(movies);
+        List<Movie> recommendedMovies = recommendationSystem.recommendMovies(user);
+
+        // Display recommended movies
+        if (recommendedMovies.isEmpty()) {
+            System.out.println("Sorry, no movies found matching your interests.");
+        } else {
+            System.out.println("Recommended movies:");
+            for (Movie movie : recommendedMovies) {
+                System.out.println(movie.getTitle());
+            }
+        }
+    }
+}
---
+++ sprint1/READme.md
@@ -0,0 +1,24 @@
+# Overview
+
+To further my learning I spent a lot of time in the oast few weeks working on learning how to code better with Java. I learned a lot about the language and how to use it.
+
+The program that asks the user for an input of different genres of movies that they likes and outputs different movies that the user could watch!
+
+I wrote this software so that I can have a program that I can modify and add more movies/genres to so that when I am looking for a movie to watch I can find one quickly. I also want to be able to modify it more in the future so it works better and is a lot longer. I think that I also would want to modify it to have more things like ratings, actors, and music that is in the film.
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+I used Java to create my program and the Java.util library.
+
+# Useful Websites
+- IMdb (https://www.imdb.com/?ref_=nv_home)
+- W3schools (https://www.w3schools.com/java/)
+
+# Future Work
+
+- Add in ratings
+- Add in actors
+- Add in famous music as an option 
